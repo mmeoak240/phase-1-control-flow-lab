48512e89ecb444a2cbfb5528c8c1d03fb3b6218b
Michael Meoak <mmeoak240@gmail.com>
2022-02-22 18:40:54 -0700

    commit tree 4897e162a95fabf26c7ac38932e3f2997babe206

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if(distance <= 400) {
+    return 'This one is on me!'
+  } else if(distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+   } else if(distance > 2500) {
+     return 'No can do.'
+    }
+  }
+
+
+function ternaryCheckCity(destination){
+ return (destination === 'NYC' ? "Ok, sounds good." : "No go.")
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
